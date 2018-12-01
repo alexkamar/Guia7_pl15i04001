31757325e9acb83947eb62e7260169550a4a1268
alexkamar <42698288+alexkamar@users.noreply.github.com>
2018-11-30 22:41:33 -0600

    GUIA7_PL15I04001

---
+++ src/main/java/com/alexkamar/guia7/Modelo/Categoria.java
@@ -0,0 +1,19 @@
+package com.alexkamar.guia7.Modelo;
+
+public class Categoria {
+    String id_categoria;
+    String nombre;
+
+    public Categoria(String id_categoria, String nombre) {
+        this.id_categoria = id_categoria;
+        this.nombre = nombre;
+    }
+
+    public String getId_categoria() {
+        return id_categoria;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+}
